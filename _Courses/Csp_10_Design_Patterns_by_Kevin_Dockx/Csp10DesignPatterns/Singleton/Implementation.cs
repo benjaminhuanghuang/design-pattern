d1aead2b505b8cd8e9accf031f6e8338d8de264d
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-03-01 21:52:51 -0800

    update

---
+++ _Courses/Csp_10_Design_Patterns_by_Kevin_Dockx/Csp10DesignPatterns/Singleton/Implementation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        /*
+         *  .Net Lazy<T> provide threa-safe manner, compiler takes care of making this thread-safe
+         *   new Logger() when first access the _lazyLogger.Value
+         */
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static Logger Instance
+        {
+            get { return _lazyLogger.Value; }
+        }
+
+        // protected allow subclassing the Logger
+        protected Logger()
+        {
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary> 
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
---
+++ _Courses/Csp_10_Design_Patterns_by_Kevin_Dockx/README.md
@@ -0,0 +1,14 @@
+# C# 10 Design Patterns
+by Kevin Dockx
+https://www.kevindockx.com/
+
+https://app.pluralsight.com/library/courses/c-sharp-10-design-patterns/
+https://github.com/kevinDockx/CSharp10DesignPatterns
+
+- Intent of the pattern
+- Structure of the pattern
+- Real-life pattern implementation
+- Pattern use cases, consequences, advantages/disadvantages, related patterns
+
+Don't learn the pattern implementation from the top of your head, learn which problem a pattern solves
+
