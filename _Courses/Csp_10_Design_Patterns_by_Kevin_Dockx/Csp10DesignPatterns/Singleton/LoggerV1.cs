f245082e3b26b50203b4fa93598d18d00d4b0874
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-03-01 21:56:13 -0800

    update

--- _Courses/Csp_10_Design_Patterns_by_Kevin_Dockx/Csp10DesignPatterns/Singleton/LoggerV1.cs
+++ _Courses/Csp_10_Design_Patterns_by_Kevin_Dockx/Csp10DesignPatterns/Singleton/LoggerV1.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Singleton
 {
-    internal class LoggerV1
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class LoggerV1
     {
+        private static LoggerV1? _instance;
+
+        
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static LoggerV1 Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        protected LoggerV1()
+        {
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary> 
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
     }
 }
